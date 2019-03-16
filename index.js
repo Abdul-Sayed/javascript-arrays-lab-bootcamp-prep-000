33a7f71f60f817e66c19890a9b639f1c96709a90
Abdul <awsayed@alaska.edu>
2019-03-16 20:18:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,8 +3,13 @@ var kittens = ['Milo', 'Otis', 'Garfield'];
 // Add your functions and code here
 
 function destructivelyAppendKitten(name) {
-  
+  return kittens.push(name);
 }
+
+function destructivelyPrependKitten(name) {
+  return kittens.unshift(name);
+}
+
 
 function appendKitten(name) {
   return kittens.concat(name);
